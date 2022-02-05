5a965e57c6fb2102c380f505607988c0e784201d
Matthew Adcock <43476212+MattA-Official@users.noreply.github.com>
2022-02-05 23:25:57 +0000

    fix: comment out file

--- lib/rate-limit.ts
+++ lib/rate-limit.ts
@@ -1,32 +1,32 @@
-import LRU from "lru-cache";
+// import LRU from "lru-cache";
 
-export default async function rateLimit(options: {
-  uniqueTokenPerInterval: any;
-  interval: any;
-}) {
-  const tokenCache = new LRU({
-    max: parseInt(options.uniqueTokenPerInterval || 500, 10),
-    maxAge: parseInt(options.interval || 60000, 10),
-  });
+// export default async function rateLimit(options: {
+//   uniqueTokenPerInterval: any;
+//   interval: any;
+// }) {
+//   const tokenCache = new LRU({
+//     max: parseInt(options.uniqueTokenPerInterval || 500, 10),
+//     maxAge: parseInt(options.interval || 60000, 10),
+//   });
 
-  return {
-    check: (res, limit, token) =>
-      new Promise((resolve, reject) => {
-        const tokenCount = tokenCache.get(token) || [0];
-        if (tokenCount[0] === 0) {
-          tokenCache.set(token, tokenCount);
-        }
-        tokenCount[0] += 1;
+//   return {
+//     check: (res, limit, token) =>
+//       new Promise((resolve, reject) => {
+//         const tokenCount = tokenCache.get(token) || [0];
+//         if (tokenCount[0] === 0) {
+//           tokenCache.set(token, tokenCount);
+//         }
+//         tokenCount[0] += 1;
 
-        const currentUsage = tokenCount[0];
-        const isRateLimited = currentUsage >= parseInt(limit, 10);
-        res.setHeader("X-RateLimit-Limit", limit);
-        res.setHeader(
-          "X-RateLimit-Remaining",
-          isRateLimited ? 0 : limit - currentUsage
-        );
+//         const currentUsage = tokenCount[0];
+//         const isRateLimited = currentUsage >= parseInt(limit, 10);
+//         res.setHeader("X-RateLimit-Limit", limit);
+//         res.setHeader(
+//           "X-RateLimit-Remaining",
+//           isRateLimited ? 0 : limit - currentUsage
+//         );
 
-        return isRateLimited ? reject() : resolve();
-      }),
-  };
-}
+//         return isRateLimited ? reject() : resolve();
+//       }),
+//   };
+// }
